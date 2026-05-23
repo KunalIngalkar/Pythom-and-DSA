aba3442720b46929f2b72e575d85e2cac6d4f169
KunalIngalkar <Kunal.Ingalkar24@gmail.com>
2026-05-23 12:56:03 +0530

    Update dict.py

--- Python-Dictionary/dict.py
+++ Python-Dictionary/dict.py
@@ -9,12 +9,12 @@ You are analyzing raw logs from an API gateway. The logs are provided as a list 
 dictionaries, where each dictionary represents a single API request. Your task is to
 build a nested dictionary that aggregates the count of HTTP status codes returned
 for each endpoint.
-
 Write a function that takes this list and returns a dictionary where the keys are the
 endpoint strings, and the values are dictionaries mapping the status_code to its
 occurrence count.
 
-Sample Input
+'''
+
 logs = [
  {"endpoint": "/api/users", "status": 200},
  {"endpoint": "/api/users", "status": 200},
@@ -24,20 +24,14 @@ logs = [
  {"endpoint": "/api/payments", "status": 500}
 ]
 
-'''
 def count_status_codes(logs):
     result = {}
-
     for log in logs:
         endpoint = log["endpoint"]
-        status = log["status_code"]
-
+        status = log["status"]
         if endpoint not in result:
             result[endpoint] = {}
-
         if status not in result[endpoint]:
             result[endpoint][status] = 0
-
         result[endpoint][status] += 1
-
     return result
